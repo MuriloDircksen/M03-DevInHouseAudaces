1472de060261ec882ed5ad6081e451c7ad273999
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-09-08 14:40:11 -0300

    adicionando campo post

--- Semana 9/RedeSocial/schema.js
+++ Semana 9/RedeSocial/schema.js
@@ -19,11 +19,29 @@ const users = [
     {id: 3, name: "Jose Saramago", age: 23, profession: "dev3"},
     {id: 4, name: "Maria Antonieta", age: 29, profession: "dev2"}
     ];
+
+const PostType = new GraphQLObjectType({ //GraphQLObjectType tipo genericco
+    name: 'Post',
+    description: 'Representa os dados de um post',
+    fields: ()=> ({
+        id: {type: GraphQLNonNull(GraphQLString)}, 
+        title:{type: GraphQLString},
+        content: {type: GraphQLString},
+        publishDate: {type: GraphQLString}
+    })
+})
+
+const posts = [
+    {id: 1, title: "Olá Mundo", content: "Novo olá", publishDate: "23/03/2023"},
+    {id: 2, title: "Olá Brasil", content: "Novo olá Brasil", publishDate: "23/03/2023"},
+    {id: 3, title: "Olá Europa", content: "Novo olá Europa", publishDate: "23/03/2023"}
+]; 
 const RootQuery = new GraphQLObjectType({ //criar a query
     name: "RootQueryType",
     fields:{
        user:{
             type: UserType,
+            description: "The user",
             args:{
                 id: {type: GraphQLString}
             }, //definir o que quer utilizar como parametro de argumento
@@ -33,8 +51,24 @@ const RootQuery = new GraphQLObjectType({ //criar a query
         },
         users:{
             type: new GraphQLList(UserType),
+            description: "List of all users",
             resolve: (parent, args)=>{
                 return users
+            }
+        },
+        posts: {
+            type: new GraphQLList(PostType),
+            description: "List of all posts",
+            resolve: () => posts,
+        },
+        post:{
+            type: PostType,
+            description: "The post",
+            args:{
+                id: {type: GraphQLString}
+            }, 
+            resolve(parent, args){
+                return posts.find(post => post.id == args.id)
             }
         }
     }
